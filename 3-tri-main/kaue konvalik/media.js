7671178d1993dac712bd3be17b6a10bf4b3af8e1
Konevalik <136601254+Konevalik@users.noreply.github.com>
2023-09-18 08:15:51 -0300

    Add files via upload

---
+++ 3-tri-main/kaue konvalik/media.js
@@ -0,0 +1,4 @@
+//Criar frequencia ao form e elementos de resposta do progama
+const form = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
